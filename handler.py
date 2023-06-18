88ceea6889941cd252d6555c8e03decdb6d73162
iagotannusb <iagotannusb.pomponet@gmail.com>
2023-06-18 10:38:37 -0300

    editado handler.py

--- handler.py
+++ handler.py
@@ -43,6 +43,5 @@ def rossmann_predict():
         return Response('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
+   app.run ('0.0.0.0') 
     
